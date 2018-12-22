1d578de1043de4dbdc88ef11cc3c727656448587
pebkm <pebkm@rediffmail.com>
2018-12-22 09:33:27 +0100

    Update

--- test.groovy
+++ test.groovy
@@ -1,7 +1,7 @@
 class test{
     static void main(String [] args){
      //   println("Test");
-    def name = "Tester";
+    def name = "Tester Eshu";
     println (name)
     name = 40
     println (name)
